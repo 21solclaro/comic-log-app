68e4e8767a82a2fcc85d6a912b27e3b17b4b691d
Yoh <yokeus2000@gmail.com>
2022-08-06 16:40:59 +0900

    update: スクロール機能を修正

--- lib/pages/comic_page.dart
+++ lib/pages/comic_page.dart
@@ -1,4 +1,8 @@
 import 'package:comic_log_app/models/comic.dart';
+import 'package:comic_log_app/widgets/tabs/about_tab.dart';
+import 'package:comic_log_app/widgets/tabs/books_tab.dart';
+import 'package:comic_log_app/widgets/tabs/chapters_tab.dart';
+import 'package:comic_log_app/widgets/tabs/related_works_tab.dart';
 import 'package:flutter/material.dart';
 
 class ComicPage extends StatelessWidget {
@@ -9,78 +13,77 @@ class ComicPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 0,
-      length: 4,
+      length: _tabs.length,
       child: Scaffold(
-          body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 60, 10, 20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 140,
-                  width: 100,
-                  color: Colors.grey,
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                Flexible(
-                  child: Text(
-                    comic.title,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                pinned: true,
+                expandedHeight: 300,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        comic.imgUrl == ''
+                            ? Container(
+                                height: 140,
+                                width: 100,
+                                color: Colors.grey,
+                              )
+                            : Image(image: NetworkImage(comic.imgUrl)),
+                        const SizedBox(width: 20),
+                        Flexible(
+                          child: FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: Text(
+                              comic.title,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
-            ),
-          ),
-          Container(
-            height: 45,
-            decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(25.0)),
-            child: TabBar(
-              indicator: BoxDecoration(
-                  color: Colors.green[300],
-                  borderRadius: BorderRadius.circular(25.0)),
-              labelColor: Colors.white,
-              unselectedLabelColor: Colors.black,
-              tabs: const [
-                Tab(
-                  text: '詳細',
+                bottom: TabBar(
+                  indicator: BoxDecoration(
+                      color: Colors.green[300],
+                      borderRadius: BorderRadius.circular(25.0)),
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Colors.black,
+                  tabs: const [
+                    Tab(
+                      text: '詳細',
+                    ),
+                    Tab(
+                      text: '巻',
+                    ),
+                    Tab(
+                      text: '話',
+                    ),
+                    Tab(
+                      text: '関連作品',
+                    )
+                  ],
                 ),
-                Tab(
-                  text: '巻',
-                ),
-                Tab(
-                  text: '話',
-                ),
-                Tab(
-                  text: '関連作品',
-                )
-              ],
-            ),
-          ),
-          const Expanded(
-              child: TabBarView(
-            children: [
-              Center(
-                child: Text('詳細'),
               ),
-              Center(
-                child: Text('巻'),
-              ),
-              Center(
-                child: Text('話'),
-              ),
-              Center(
-                child: Text('関連作品'),
-              )
-            ],
-          ))
-        ],
-      )),
+            ];
+          },
+          body: TabBarView(children: _tabs),
+        ),
+      ),
     );
   }
 }
+
+final _tabs = [
+  const AboutTab(),
+  const BooksTab(),
+  const ChaptersTab(),
+  const RelatedWorksTab()
+];

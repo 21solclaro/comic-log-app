fe51c430039f6ba343dd013cc2f14587bf5d4dc9
Yoh <yokeus2000@gmail.com>
2022-08-06 16:38:42 +0900

    add: グリッドビューと切り替えボタンを追加

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,41 +1,158 @@
+import 'package:comic_log_app/models/book.dart';
+import 'package:comic_log_app/models/chapter.dart';
 import 'package:comic_log_app/models/comic.dart';
 import 'package:comic_log_app/pages/comic_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class Home extends StatelessWidget {
+final isListViewProvider = StateProvider<bool>((ref) => true);
+
+class Home extends ConsumerWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isListView = ref.watch(isListViewProvider);
     return Scaffold(
-      body: ListView.builder(
-          itemCount: comics.length,
-          itemBuilder: ((context, index) {
-            return ListTile(
-              title: Text(comics[index].title),
-              subtitle: Text(comics[index].author),
-              leading: Container(
-                height: 60,
-                width: 40,
-                color: Colors.grey,
-              ),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                    return ComicPage(comic: comics[index]);
-                  }),
-                );
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: () {
+                ref.read(isListViewProvider.notifier).state = true;
               },
-            );
-          })),
-    );
+            ),
+            IconButton(
+              icon: const Icon(Icons.grid_view),
+              onPressed: () {
+                ref.read(isListViewProvider.notifier).state = false;
+              },
+            ),
+          ],
+        ),
+        body: isListView
+            ? ListView.builder(
+                itemCount: comics.length,
+                itemBuilder: ((context, index) {
+                  return ListTile(
+                    title: Text(comics[index].title),
+                    subtitle: Row(
+                      children: [
+                        for (int i = 0;
+                            i < comics[index].author.length;
+                            i++) ...{
+                          if (i > 0) ...{
+                            Text('・${comics[index].author[i]}')
+                          } else
+                            Text('${comics[index].author[i]}')
+                        }
+                      ],
+                    ),
+                    leading: comics[index].imgUrl == ''
+                        ? Container(
+                            height: 60,
+                            width: 40,
+                            color: Colors.grey,
+                          )
+                        : Image(image: NetworkImage(comics[index].imgUrl)),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) {
+                          return ComicPage(comic: comics[index]);
+                        }),
+                      );
+                    },
+                  );
+                }))
+            : GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                ),
+                itemCount: comics.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) {
+                          return ComicPage(comic: comics[index]);
+                        }),
+                      );
+                    },
+                    child: SizedBox(
+                        height: 200,
+                        width: 100,
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 90,
+                              width: 60,
+                              color: Colors.grey,
+                            ),
+                            Text(
+                              comics[index].title,
+                              style: const TextStyle(fontSize: 10),
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          ],
+                        )),
+                  );
+                }));
   }
 }
 
 final comics = [
-  Comic(title: 'ONE PIECE', author: '尾田栄一郎', imgUrl: '', startYear: 1997),
-  Comic(title: 'ジョジョの奇妙な冒険', author: '荒木飛呂彦', imgUrl: '', startYear: 1986),
-  Comic(title: 'HUNTERxHUNTER', author: '冨樫義博', imgUrl: '', startYear: 1998),
-  Comic(title: '嘘喰い', author: '迫稔雄', imgUrl: '', startYear: 2006),
-  Comic(title: 'こちら葛飾区亀有公園前派出所', author: '秋本治', imgUrl: '', startYear: 1976)
+  Comic(
+      title: 'ONE PIECE',
+      author: ['尾田栄一郎'],
+      imgUrl:
+          'https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088725093/240/9784088725093.jpg',
+      publisher: '集英社',
+      magazine: '週刊少年ジャンプ',
+      startYear: 1997,
+      books: [
+        Book(
+            volume: 1,
+            imgUrl:
+                'https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088725093/240/9784088725093.jpg',
+            publishedAt: DateTime(1997, 12, 24),
+            isRead: true),
+        Book(
+            volume: 2,
+            imgUrl:
+                'https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088725444/240/9784088725444.jpg',
+            publishedAt: DateTime(1998, 4, 3),
+            isRead: true),
+        Book(
+            volume: 3,
+            imgUrl:
+                'https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088725697/240/9784088725697.jpg',
+            publishedAt: DateTime(1998, 6, 4),
+            isRead: true),
+      ],
+      chapters: [
+        Chapter(volume: 1, chapterTitle: 'ROMANCE DAWN －冒険の夜明け－', isRead: true),
+        Chapter(volume: 2, chapterTitle: 'その男〝麦わらのルフィ〟', isRead: true),
+        Chapter(volume: 3, chapterTitle: '〝海賊狩りのゾロ〟登場', isRead: true),
+      ]),
+  Comic(
+      title: 'ジョジョの奇妙な冒険 PART7\nスティール・ボール・ラン',
+      author: ['荒木飛呂彦'],
+      imgUrl:
+          'https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088736013/240/9784088736013.jpg',
+      publisher: '集英社',
+      magazine: 'ウルトラジャンプ',
+      startYear: 1986,
+      books: [],
+      chapters: []),
+  Comic(
+      title: 'DEATH NOTE',
+      author: ['大場つぐみ', '小畑健'],
+      // imgUrl:
+      //     'https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088736211/240/9784088736211.jpg',
+      imgUrl: '',
+      publisher: '集英社',
+      magazine: '週刊少年ジャンプ',
+      startYear: 2003,
+      books: [],
+      chapters: []),
 ];

5441c2fd4c02b737ccd147c48bdadf1f1a225142
Yoh <yokeus2000@gmail.com>
2022-08-06 19:38:55 +0900

    add: BookとChapterのリストを追加

--- lib/widgets/tabs/books_tab.dart
+++ lib/widgets/tabs/books_tab.dart
@@ -1,12 +1,19 @@
+import 'package:comic_log_app/models/comic.dart';
 import 'package:flutter/material.dart';
 
 class BooksTab extends StatelessWidget {
-  const BooksTab({Key? key}) : super(key: key);
+  const BooksTab({Key? key, required this.comic}) : super(key: key);
+  final Comic comic;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('巻'),
-    );
+    return ListView.builder(
+        itemCount: comic.books.length,
+        itemBuilder: ((context, index) {
+          return ListTile(
+            leading: Image(image: NetworkImage(comic.books[index].imgUrl)),
+            title: Text('${comic.books[index].volume}巻'),
+          );
+        }));
   }
 }
--- lib/widgets/tabs/chapters_tab.dart
+++ lib/widgets/tabs/chapters_tab.dart
@@ -1,12 +1,20 @@
+import 'package:comic_log_app/models/comic.dart';
 import 'package:flutter/material.dart';
 
 class ChaptersTab extends StatelessWidget {
-  const ChaptersTab({Key? key}) : super(key: key);
+  const ChaptersTab({Key? key, required this.comic}) : super(key: key);
+
+  final Comic comic;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('話'),
-    );
+    return ListView.builder(
+        itemCount: comic.chapters.length,
+        itemBuilder: ((context, index) {
+          return ListTile(
+            leading: Text('${comic.chapters[index].volume}話'),
+            title: Text(comic.chapters[index].chapterTitle),
+          );
+        }));
   }
 }

f0002edeb17e5f84beb6f5d8515f992b0c2715de
Yoh <yokeus2000@gmail.com>
2022-08-08 19:45:14 +0900

    add: セッティングページを追加

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Setting'),
+    return SettingsList(
+      lightTheme: const SettingsThemeData(
+          leadingIconsColor: Colors.white,
+          settingsListBackground: Colors.white,
+          settingsSectionBackground: Colors.grey),
+      sections: [
+        SettingsSection(
+          title: const Text('Common'),
+          tiles: <SettingsTile>[
+            SettingsTile.navigation(
+              leading: const Icon(Icons.language),
+              title: const Text('Language'),
+              value: const Text('English'),
+            ),
+            SettingsTile.switchTile(
+              onToggle: (value) {},
+              initialValue: true,
+              leading: const Icon(Icons.dark_mode_outlined),
+              title: const Text('Dark Theme'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }

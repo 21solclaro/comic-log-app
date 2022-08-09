1d050b3184dfac67dd7394896c5ae1c7b42606ae
Yoh <yokeus2000@gmail.com>
2022-08-09 15:07:54 +0900

    add: 詳細タブに要素を追加

--- lib/widgets/tabs/about_tab.dart
+++ lib/widgets/tabs/about_tab.dart
@@ -1,12 +1,68 @@
+import 'package:comic_log_app/models/comic.dart';
 import 'package:flutter/material.dart';
 
 class AboutTab extends StatelessWidget {
-  const AboutTab({Key? key}) : super(key: key);
+  const AboutTab({Key? key, required this.comic}) : super(key: key);
+
+  final Comic comic;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('詳細'),
+    return Column(
+      children: [
+        ListTile(
+          title: Row(
+            children: [
+              for (int i = 0; i < comic.author.length; i++) ...{
+                if (i > 0) ...{
+                  Text(
+                    '・${comic.author[i]}',
+                    style: const TextStyle(fontSize: 14),
+                  )
+                } else
+                  Text(
+                    '${comic.author[i]}',
+                    style: const TextStyle(fontSize: 14),
+                  )
+              }
+            ],
+          ),
+          leading: const Icon(Icons.people_alt_outlined),
+        ),
+        ListTile(
+          title: Text(
+            comic.publisher,
+            style: const TextStyle(fontSize: 14),
+          ),
+          leading: const Icon(Icons.business_outlined),
+        ),
+        ListTile(
+          title: Text(
+            comic.magazine,
+            style: const TextStyle(fontSize: 14),
+          ),
+          leading: const Icon(Icons.newspaper),
+        ),
+        ListTile(
+          title: Text(
+            '${comic.startYear}年〜${comic.serializeState == SerializeState.finished ? comic.endYear : DateTime.now().year}年',
+            style: const TextStyle(fontSize: 14),
+          ),
+          leading: const Icon(Icons.timeline),
+        ),
+        ListTile(
+          title: Text(
+            '${comic.books.length}巻・${comic.chapters.length}話',
+            style: const TextStyle(fontSize: 14),
+          ),
+          leading: const Icon(Icons.menu_book),
+        ),
+        const Divider(
+          indent: 16,
+          endIndent: 16,
+          thickness: 1,
+        )
+      ],
     );
   }
 }

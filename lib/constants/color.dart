21ca0855646296aee1d197fad516aac3e5f11138
Yoh <yokeus2000@gmail.com>
2022-08-16 11:49:29 +0900

    update: テーマを修正

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+//ライトテーマ用の色
 Color lMainColor = Colors.deepPurple;
+Color lForegroundColor = const Color(0xFFFFFFFF);
 Color lBackgroundColor = const Color(0xFFEFEFF4);
 
+//ダークテーマ用の色
 Color dMainColor = Colors.deepOrange;
-Color dSubColor = const Color(0xCC171717);
+Color dForegroundColor = const Color(0x2C767680);
 Color dBackgroundColor = const Color(0xFF171717);
 
+//共通の色
 Color iconGrey = Colors.blueGrey;
--- lib/constants/theme_data.dart
+++ lib/constants/theme_data.dart
@@ -2,27 +2,27 @@ import 'package:comic_log_app/constants/color.dart';
 import 'package:flutter/material.dart';
 
 final ThemeData lightThemeData = ThemeData.light().copyWith(
-  scaffoldBackgroundColor: lBackgroundColor,
-  appBarTheme: AppBarTheme(
-      actionsIconTheme: IconThemeData(color: lMainColor),
-      backgroundColor: lForegroundColor),
-  bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    backgroundColor: lForegroundColor,
-    selectedItemColor: lMainColor,
-    unselectedItemColor: iconGrey,
-    showUnselectedLabels: false,
-  ),
-);
+    scaffoldBackgroundColor: lBackgroundColor,
+    appBarTheme: AppBarTheme(
+        actionsIconTheme: IconThemeData(color: lMainColor),
+        backgroundColor: lForegroundColor),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: lForegroundColor,
+      selectedItemColor: lMainColor,
+      unselectedItemColor: iconGrey,
+      showUnselectedLabels: false,
+    ),
+    cardTheme: CardTheme(color: lForegroundColor));
 
 final ThemeData darkThemeData = ThemeData.dark().copyWith(
-  scaffoldBackgroundColor: dBackgroundColor,
-  appBarTheme: AppBarTheme(
-      iconTheme: IconThemeData(color: dMainColor),
-      backgroundColor: dForegroundColor),
-  bottomNavigationBarTheme: BottomNavigationBarThemeData(
-    backgroundColor: dForegroundColor,
-    selectedItemColor: dMainColor,
-    unselectedItemColor: iconGrey,
-    showUnselectedLabels: false,
-  ),
-);
+    scaffoldBackgroundColor: dBackgroundColor,
+    appBarTheme: AppBarTheme(
+        iconTheme: IconThemeData(color: dMainColor),
+        backgroundColor: dForegroundColor),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: dForegroundColor,
+      selectedItemColor: dMainColor,
+      unselectedItemColor: iconGrey,
+      showUnselectedLabels: false,
+    ),
+    cardTheme: CardTheme(color: dForegroundColor));

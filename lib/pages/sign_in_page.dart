6630044bc68357cfe1c1f12d849a6de4016197e4
Yoh <yokeus2000@gmail.com>
2022-09-06 23:28:17 +0900

    refactor: SignButtonに統一

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,11 +1,9 @@
-import '../providers/sign_provider.dart';
 import '../widgets/buttons/custom_outlined_button.dart';
+import '../widgets/buttons/sign_button.dart';
 import '../widgets/text_field/email_field.dart';
 import '../widgets/text_field/password_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'root_page.dart';
 
 class SignInPage extends ConsumerWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -37,63 +35,16 @@ class SignInPage extends ConsumerWidget {
                 ],
               ),
             ),
-            SignInButton(formKey: signInFormKey, text: 'Sign In'),
-            // const CustomElevatedButton(text: 'Sign In', function: _signIn),
+            SignButton(
+              formKey: signInFormKey,
+              text: 'Sign In',
+              errorMessage: 'ログインに失敗しました。',
+            ),
             const SizedBox(height: 10),
             CustomOutlinedButton(
                 text: 'Back', function: () => Navigator.pop(context))
           ],
         ),
-      ),
-    );
-  }
-}
-
-class SignInButton extends ConsumerWidget {
-  const SignInButton({Key? key, required this.formKey, required this.text})
-      : super(key: key);
-
-  final GlobalKey<FormState> formKey;
-  final String text;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final email = ref.watch(emailProvider);
-    final password = ref.watch(passwordProvider);
-
-    return SizedBox(
-      height: 50,
-      width: double.infinity,
-      child: ElevatedButton(
-        onPressed: () async {
-          if (!formKey.currentState!.validate()) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text('Please fill out all forms correctly.'),
-              backgroundColor: Colors.red,
-            ));
-            return;
-          }
-          try {
-            // await auth.signInWithEmailAndPassword(
-            //     email: email.text, password: password.text);
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => const RootPage()));
-            email.clear();
-            password.clear();
-          } catch (e) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text(
-                  'Sign in failed. Please fill out all forms correctly or sign up.'),
-              backgroundColor: Colors.red,
-            ));
-          }
-        },
-        style: ElevatedButton.styleFrom(
-          primary: Colors.black,
-          shape: const StadiumBorder(),
-          textStyle: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        child: Text(text),
       ),
     );
   }
--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -1,12 +1,10 @@
-import '../providers/sign_provider.dart';
 import '../widgets/buttons/custom_outlined_button.dart';
+import '../widgets/buttons/sign_button.dart';
 import '../widgets/text_field/email_field.dart';
 import '../widgets/text_field/password_field.dart';
 import '../widgets/text_field/username_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'root_page.dart';
 
 class SignUpPage extends ConsumerWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -40,73 +38,16 @@ class SignUpPage extends ConsumerWidget {
               ),
             ),
             const SizedBox(height: 30),
-            SignUpButton(formKey: signUpFormKey, text: 'Sign Up'),
-            // const CustomElevatedButton(text: 'Sign Up', function: _signUp),
+            SignButton(
+              formKey: signUpFormKey,
+              text: 'Sign Up',
+              errorMessage: '登録に失敗しました。',
+            ),
             const SizedBox(height: 10),
             CustomOutlinedButton(
                 text: 'Back', function: () => Navigator.pop(context))
           ],
         ),
-      ),
-    );
-  }
-}
-
-class SignUpButton extends ConsumerWidget {
-  const SignUpButton({Key? key, required this.formKey, required this.text})
-      : super(key: key);
-
-  final GlobalKey<FormState> formKey;
-  final String text;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final username = ref.watch(usernameProvider);
-    final email = ref.watch(emailProvider);
-    final password = ref.watch(passwordProvider);
-
-    return SizedBox(
-      height: 50,
-      width: double.infinity,
-      child: ElevatedButton(
-        onPressed: () async {
-          if (!formKey.currentState!.validate()) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text('Please fill out all forms correctly.'),
-              backgroundColor: Colors.red,
-            ));
-            return;
-          }
-          try {
-            // final result = await auth.createUserWithEmailAndPassword(
-            //     email: email.text, password: password.text);
-            // final user = model.User(
-            //     uid: result.user!.uid,
-            //     userImg: '',
-            //     userName: result.user!.email!.split('@')[0],
-            //     createdAt: DateTime.now(),
-            //     updatedAt: DateTime.now());
-
-            // await collection.doc(user.uid).set(user.toJson());
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => const RootPage()));
-            username.clear();
-            email.clear();
-            password.clear();
-          } catch (e) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text(
-                  'Sign Up failed. Please fill out all forms correctly or sign in.'),
-              backgroundColor: Colors.red,
-            ));
-          }
-        },
-        style: ElevatedButton.styleFrom(
-          primary: Colors.black,
-          shape: const StadiumBorder(),
-          textStyle: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        child: Text(text),
       ),
     );
   }
